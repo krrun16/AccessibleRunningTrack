0e774c02ada6d6bbc059efa00ddfaf6a8bf34e99
Rumi <rumi@uiowa.edu>
2019-02-21 20:45:10 -0600

    Handling permission from StartActivity
    Shift permission from MainActivity to StartActivity to avoid exception

--- CustomVision/StartActivity.cs
+++ CustomVision/StartActivity.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-
+using Android;
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
+using Android.Support.V4.App;
 using Android.Support.V7.App;
 using Android.Widget;
 
@@ -12,21 +14,35 @@ namespace CustomVision
     [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/MyTheme")]
     internal class StartActivity : AppCompatActivity
     {
+        public static int PERMISSION_ALL = 0;
         private Button startButton;
         private Button StartButton => startButton ?? (startButton = FindViewById<Button>(Resource.Id.start_btn));
+
+        private static readonly string[] permissions = {
+            Manifest.Permission.WriteExternalStorage,
+            Manifest.Permission.Camera
+        };
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            ActivityCompat.RequestPermissions(this, permissions, PERMISSION_ALL);
 
             SetContentView(Resource.Layout.start_btn);
-
+           
             StartButton.Click += StartButton_Click;
+             
+
+               
+            
         }
 
         private void StartButton_Click(object sender, EventArgs e) {
-            Intent m_activity = new Intent(this, typeof(MainActivity));
-            StartActivity(m_activity);
+            
+                Intent m_activity = new Intent(this, typeof(MainActivity));
+                StartActivity(m_activity);
+            
+            
         }
     }
 }
